f8c79b25c8b999471e307030b4248ca8b12f9d6f
rianfernandesno <zrpapz12@gmail.com>
2025-05-30 12:59:36 -0300

    Start Project

---
+++ frontend - desktop/main.js
@@ -0,0 +1,18 @@
+const {app, BrowserWindow} = require('electron');
+
+let mainWindow;
+
+function createWindow() {
+    mainWindow = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            nodeIntegration: true
+        },
+        autoHideMenuBar: true
+    });
+
+    mainWindow.loadFile('index.html');
+};
+
+app.whenReady().then(createWindow);
