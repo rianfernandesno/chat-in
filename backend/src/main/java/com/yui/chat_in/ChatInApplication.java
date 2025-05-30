015e83b5f691b6256927eb6006419e4f1d5de173
rianfernandesno <zrpapz12@gmail.com>
2025-05-30 15:02:29 -0300

    Backend Spring ok

---
+++ backend/src/main/java/com/yui/chat_in/ChatInApplication.java
@@ -0,0 +1,13 @@
+package com.yui.chat_in;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatInApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatInApplication.class, args);
+	}
+
+}
